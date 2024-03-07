c980c9e523735da7049b6eeb1a91977cab763fa4
vengatesh221999 <157572942+vengatesh221999@users.noreply.github.com>
2024-03-07 16:12:50 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors:{
+      'violet':'#9d79e0ff',
+      'background':'#f2f3f7ff',
+      'active_back':"#ebecf0ff",
+       'white':'#fcfcfcff',
+      'letter-gray':'#72757dff',
+      'letter-black':'#2b2b2bff',
+      'footer':'#1c1819ff',
+      'red':'#e88274ff',
+      'blue':'#4973fcff',
+      'line':'#ede4e1ff',
+      'green':"#618f5eff"
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
